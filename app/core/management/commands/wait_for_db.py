108b74248653faf2202ee2a5bac410eb1d9021db
rollingsxshi <rollingsxshi@gmail.com>
2023-11-12 21:08:05 +0800

    update flake8 and add black & isort

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,11 +2,11 @@
 Django command to wait for db to be available
 """
 import time
-from psycopg2 import OperationalError as Psycopg2Error
 from typing import Any
 
-from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2Error
 
 
 class Command(BaseCommand):
@@ -14,14 +14,14 @@ class Command(BaseCommand):
 
     def handle(self, *args: Any, **options: Any) -> str | None:
         """Entrypoint for command."""
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
+                self.check(databases=["default"])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write(self.style.SUCCESS("Database available!"))
